ad856dac0f3871efe0f25190224776e281d63ad8
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-10 16:29:22 -0600

    se actualizo el archivo variables-tania

---
+++ Fundamentos-de-phyton/HOLA-MUNDO.py
@@ -0,0 +1,5 @@
+#Tania Garcia Flores.
+#8 de Octubre del 2024.
+#inicializacion a la programacion en phyton.
+print("Hola Mundo")#El print sirve para imprimir algo,para abrir la consola se debe
+# ir al simbolo de play.
