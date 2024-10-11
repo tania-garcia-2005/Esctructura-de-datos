f50e6d5d04c919715a7c19a29eb2d365f21962fd
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-11 17:20:53 -0600

    SUBI UN PROGRAMA EL CUAL ME DA EL REGISTRO DE PRESUPUESTO DE 2 DIAS

---
+++ Fundamentos-de-phyton/Tipos_de_datos_ejercicios.py
@@ -0,0 +1,71 @@
+#Tania Garcia Flores
+#25 de Agosto de 2005.
+'''''
+Descripción:
+Usos de los tipos básico de datos en Python.
+'''
+from pickle import FALSE
+
+# Notas:
+"""
+En Python, no se requiere indicar el tipo de la variable en su declaración.
+Los valores básicos que pueden almacenar las variables son:
+- Int
+- Float
+- String (str)
+- Boolean. True o False (con inicial mayúscula).
+- None. Tipo especial que representa una ausencia de valor.
+"""
+
+# Ejercicio.
+
+# Cadena de texto
+mi_variable_texto_nombre = "Tania"
+mi_variable_texto_apellido = 'Garcia'
+print("NOMBRE:", mi_variable_texto_nombre, mi_variable_texto_apellido)
+
+# Cadena de texto
+mi_variable_texto_dia = "Lunes"    # AQUI EPECIFIQUE EL NOMBRE DE MI VARIABLE
+print("DIA:", mi_variable_texto_dia) #IMPRIME MI MENSAJE Y MI VARIABLE GUARDADA
+# Número entero
+mi_variable_entera = 20
+print("PASAJE:",mi_variable_entera)
+
+# Número decimal
+mi_variable_decimalcomida= 135.5
+print("COMIDA:", mi_variable_decimalcomida)
+
+# Número decimal
+mi_variable_otrosgastos= 34.5
+print("OTROS GASTOS:", mi_variable_otrosgastos)
+
+# Booleno
+mi_variable_booleana = True
+print('¿FUE MAYOR A MI PRESUPUESTO?:', mi_variable_booleana)
+
+# Cadena de texto
+mi_variable_texto_nombre = "Tania"
+mi_variable_texto_apellido = 'Garcia'
+print("NOMBRE:", mi_variable_texto_nombre, mi_variable_texto_apellido)
+
+# Cadena de texto
+mi_variable_texto_dia2 = "martes"
+print("Dia:", mi_variable_texto_dia2)
+# Número entero
+mi_variable_entera2 = 12
+print("Pasaje:",mi_variable_entera2)
+
+# Número decimal
+mi_variable_decimalcomida2= 45.5
+print("Comida:", mi_variable_decimalcomida2)
+
+# Número decimal
+mi_variable_otrosgastos= 4.5
+print("Otros gastos:", mi_variable_otrosgastos)
+
+# Booleno
+mi_variable_booleana = False
+print('¿FUE MAYOR A MI PRESUPUESTO?:', mi_variable_booleana)
+
+
+
