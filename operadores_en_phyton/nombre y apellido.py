fa3215e84c1f17dd8d585cf6baa206a86c9086a9
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-16 16:51:36 -0600

    imrpime el nombre y apellido

---
+++ operadores_en_phyton/nombre y apellido.py
@@ -0,0 +1,22 @@
+#Tania Garcia Flores
+#25 de Agosto de 2005.
+'''''
+Descripción:
+pide e nombre y apellido de formas distintas
+'''
+
+nombre =input("ingrese nombre:")
+apellido =input("ingrrese apellido:")
+
+
+
+
+
+
+
+
+
+
+
+
+
