f49130259515f2c81643c34351db9c53e21637a5
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-16 16:56:31 -0600

    realiza las operaciones de 2 numeros utilizando los operadores de asignacion compuesta

---
+++ operadores_en_phyton/Operadores_asiganacion_compuesta.py
@@ -0,0 +1,35 @@
+#Tania Garcia Flores
+#15 de octubre de 2024.
+
+
+
+#var_numero1+=3  #numero1=numero1
+#var_numero2-=5   #numero2=numero2
+#var_numeor1=*2
+#var_numero2 1=4
+
+
+numero1 = input("Ingresa un numero: ")
+numero2 = input("Ingresa un numero: ")
+
+num1 = float(numero1)
+num2 = float(numero2)
+
+
+num1 += num2
+print(num1)
+
+num1 *= num2
+print(num1)
+
+num1 -= num2
+print(num1)
+
+num1 += 3
+print(num1)
+
+num1 /= 2
+print(num1)
+
+#numero1=8
+#numero2=11
