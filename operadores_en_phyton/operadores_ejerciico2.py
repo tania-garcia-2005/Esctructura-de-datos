52240a392a93ee1297d507f44d24f644d104ae18
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-26 12:31:56 -0600

    Operadores Lógicos-17-Octubre-2024..

--- operadores_en_phyton/operadores_ejerciico2.py
+++ operadores_en_phyton/operadores_ejerciico2.py
@@ -1,14 +1,28 @@
-#Tania Garcia Flores
-#15 de octubre de 2024.
+#***************************************
+#****   Tania Garcia Flores.      ******
+#****   17 de octubre de 2024.    ******
+#***************************************
+
+
+# Operadores lódicos.
+'''''
+Descripción:
+Este programa determinará si el usuario pertenece a la comunidad de la Unsij.
+'''
+
+print()
+print("******Comunidad de la Unsij******")#Título.
+print()
+
 #¿Es profesor de la UNSIJ?
 #¿Es estudiante de la UNSIJ?
 #¿Forma parte de la comunidad de la UNSIJ?  true/false
 
 
 
-profesor=input("Es profesor de ela UNSIJ? si/no: ")
-alumno=input("Es alumno de ela UNSIJ? si/no: ")
+Profesor=input("Es profesor de ela UNSIJ? si/no: ")
+Alumno=input("Es alumno de ela UNSIJ? si/no: ")
 
-profesor=profesor.lower()=="si"
-alumno=alumno.lower()=="si"
-print(f"Forma parte de la comunidad UNSIJ? {profesor or alumno}")
+Profesor=Profesor.lower()=="si"#En caso de que el usuario ingrese un "mo"aparecera en pantalla false.
+Alumno=Alumno.lower()=="si"#En caso de que el usuario ingrese un "mo"aparecera en pantalla false.
+print(f"Forma parte de la comunidad UNSIJ? {Profesor or Alumno}")#Imprime True o Flase dependiendo de la respuesta que ingrese el usuario.
