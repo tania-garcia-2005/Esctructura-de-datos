f790aed383a0e7adf21f85dc58f9169b4e80b410
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-26 17:39:04 -0600

    SUbi un  programa que determinará un descuento en compras en "La cona". Fecha:26-Octubre-2024..

---
+++ Sentencias_de_Control/Sentencias_ejercicio3..py
@@ -0,0 +1,44 @@
+#***************************************
+#****   Tania Garcia Flores.      ******
+#****   26 de octubre de 2024.    ******
+#***************************************
+
+'''''
+Descripción:
+Este programa determinará un descuento en compras en "La cona".
+'''
+
+print()
+print("****** Descuentos por ser miebros de  'La cona' ******")
+print()
+
+'''
+•	Si tiene membresía, obtiene un 5 % de descuento en todas sus compras.
+•	Si tiene la membresía y su compra fue mayor o igual a $ 1000.00, entonces el descuento es del 8 %.
+•	Si no tiene la membresía, no obtiene ningún descuento y se invita a ser miembro.
+Para ello:
+a) Solicite al usuario la cantidad comprada en la tienda.
+b) Pregunte al usuario si cuenta con la membresía (Si/No).
+c) Utilice la lógica adecuada para determinar el total a pagar.
+d) Muestre el descuento y el total a pagar en consola utilizando dos decimales.
+'''
+
+
+Cantidad=float(input("Ingrese su cantidad comprada en la tienda: "))
+membresia= input("Cuenta con la membresía?(si/no): ")
+membresia=membresia.lower()=="si"
+if membresia:
+    if Cantidad >= 1000:
+        total= Cantidad * 0.08
+        pagar= Cantidad - total
+        print(f"Tu descuento es del 8% {pagar:.2f} ")
+
+    else:
+        total = Cantidad * 0.05
+        pagar = Cantidad - total
+        print(f"Tu descuento es del 5% {pagar:.2f} ")
+
+else:
+    print("No obtiene ningún descuento,te invitamos  a ser miembro.")
+
+
