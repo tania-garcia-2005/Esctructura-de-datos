ec542f6089e67a85741858acbb43d34bc266479a
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-26 13:54:58 -0600

    Subi un programa que determinará si un usuario se autentifica correctamente con su usuario y contraseña. Fecha:26-Octubre-2024..

--- operadores_en_phyton/Operadores_Ejercicio1.py
+++ operadores_en_phyton/Operadores_Ejercicio1.py
@@ -17,7 +17,7 @@ print()
 Cantidad=input("¿Realizó compras mayores a $5000.00?. (Favor de ingresar  su cantidad en decimales): ")#Pregunta al usuario si realiza compras mayores a $5000 y que ingrese la cantidad en decimales.
 Cadena= input("¿Compras a MSI?(si/no: ")#Pide al usuario que ingrese si realiza sus compras a meses sin intereses y en caso contrario debe ingresar no.
 
-Total=float(Cantidad)
+Total=float(Cantidad)#Convertí a flotante
 Variable_Bool = bool(Cantidad)
 Cadena=Cadena.lower()=="si"#En caso de que el usuario ingrese un "mo"aparecera en pantalla false.
 print(f"¿Aplica bonificación?{Variable_Bool>5000 and Cadena}")#Imprime False o True,dependiendo de la respuesta de el usuario.
