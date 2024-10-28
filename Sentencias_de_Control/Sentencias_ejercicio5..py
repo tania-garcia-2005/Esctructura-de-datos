19d31092aa5de8f2ac78e839a5fe93f1c9706cbf
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-28 17:36:01 -0600

    SUbi un  programa que determinará el promedio de una materia e indicará si el alumno aprobó o no la materia. Fecha:26-Octubre-2024..

--- Sentencias_de_Control/Sentencias_ejercicio5..py
+++ Sentencias_de_Control/Sentencias_ejercicio5..py
@@ -21,6 +21,7 @@ b) Calcule el promedio y determine si aprobó (calificación mínima de 6.0).
 d) Muestre el promedio (utilizando un decimal) y el mensaje: "¡Felicidades! Aprobaste.", o el mensaje: "Lo siento, no aprobaste.", según sea el caso.
 
 '''
+#solicita ingresar las calificaciones de sus parciales y ordinario.
 
 calificación1=float(input("Ingrese su calificacion del parcial 1: "))
 calificación2=float(input("Ingrese su calificacion del parcial 2: "))
@@ -28,9 +29,9 @@ calificación3=float(input("Ingrese su calificacion del parcial 3: "))
 ordinario=float(input("Ingrese su calificacion de su ordinario: "))
 
 
-promedio_total=(calificación1 + calificación2 + calificación3)/3
-variable=(promedio_total*0.5)+(ordinario*0.5)
-
+promedio_total=(calificación1 + calificación2 + calificación3)/3 #Calcula el promedio final sumando las primeras 3 calificaiones y las divide entre de 3.
+variable=(promedio_total*0.5)+(ordinario*0.5)#El promedio obtenido lo multiplica por 0.5 y le suma la calificacion de el ordinario multiplicado también por 0.5 (se multiplica por 0.5 por que equivale al 50% de la calificaciones la suma de los 3 parciales y el otro 50% equivale al ordinario.
+#imprime los mensajes de acuerdo al valor obtenido.
 if variable >= 6.0:
     print(f"Su promedio es de: {variable:.1f}")
     print(f"¡Felicidades! Aprobaste.")
