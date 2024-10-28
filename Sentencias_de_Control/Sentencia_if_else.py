278c702c1366032134c54d9750d95a5b1b52b2a8
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-28 17:55:55 -0600

    SUbi un  programa pide un numero y determina si el numero es par o impar Fecha:28-Octubre-2024..

---
+++ Sentencias_de_Control/Sentencia_if_else.py
@@ -0,0 +1,24 @@
+#***************************************
+#****   Tania Garcia Flores.      ******
+#****   26 de octubre de 2024.    ******
+#***************************************
+
+'''''
+Descripción:
+pide un numero y determina si el numero es par o impar
+'''
+
+
+#Codigo a ejecutar si
+#La condición es verdadera
+#Programa que determina si un número es par o impar
+
+
+
+numero=input("Ingrese un número: ")
+numero=int (numero)
+
+if numero%2==0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
