7aacfb309d1006ab33912bdc14763a3b8534ad64
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-28 17:53:57 -0600

    SUbi un  programa pide la edad y determina si es mayor de edad Fecha:28-Octubre-2024..

---
+++ Sentencias_de_Control/Sentencia_if.py
@@ -0,0 +1,27 @@
+#***************************************
+#****   Tania Garcia Flores.      ******
+#****   26 de octubre de 2024.    ******
+#***************************************
+
+'''''
+Descripción:
+pide la edad y determina si es mayor de edad
+'''
+
+#Código a ejecutar si
+#La condicion es verdadera
+
+
+#Programa que determina si eres mayor de edad
+
+variable1=input("ingrese su edad: ")
+variable1=int(variable1)
+if variable1>=18:
+    print(f"Es mayor de edad: ")
+
+
+
+#codigo que ejecuta si eres mayor de edad
+
+
+
