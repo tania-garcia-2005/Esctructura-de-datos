78a018cefdf82adfc4c17f7d9502959992f31a27
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-10-31 19:47:47 -0600

    Fecha:29-Octubre-2024..

--- Paradigmas-de-programacion-II/Examen-Parcial-1.py
+++ Paradigmas-de-programacion-II/Examen-Parcial-1.py
@@ -11,24 +11,24 @@ print()
 numero = int(input("Ingresa el número final de la cuenta: "))
 
 contador = 1
-# Recorrer los números del 1 hasta el número ingresado
+# Recorre los números del 1 hasta el número final de la cuenta.
 while contador <= numero:
     # Verificar si es múltiplo de 3 y 5
     if (contador % 3 == 0) and (contador % 5 == 0):
         contador += 1
         print("Licenciatura en Informática", end= " ")
         print()
-        # Sustituir por la frase
+        # Hace las sustituciones correspondientes por la frase.
     # Verificar si es múltiplo de 3
     elif contador % 3 == 0:
         contador += 1
-        print("Licenciatura,",end = "")  # Sustituir por la palabra
+        print("Licenciatura,",end = "")
     # Verificar si es múltiplo de 5
     elif contador % 5 == 0:
         contador += 1
-        print("Informática,",end = "")  # Sustituir por la palabra
+        print("Informática,",end = "")
 
     else:
-        print(contador,",",end = "")  # Imprimir el número
+        print(contador,",",end = "")
         contador += 1
 
