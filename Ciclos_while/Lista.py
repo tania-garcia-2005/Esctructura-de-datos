8cf64eca43d180d9400aa31b1a4408bb2d984ac8
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-11-06 21:55:22 -0600

    Fecha:29-Octubre-2024..

---
+++ Ciclos_while/Lista.py
@@ -0,0 +1,24 @@
+#***************************************
+#****   Tania Garcia Flores.      ******
+#****   6 de Noviembre 2024.      ******
+#***************************************
+
+'''''
+Descripción: Realiza una lISTA
+en esta lsita se imorimen los nombres
+
+'''
+
+print()
+print("******Programa que realiza una Lista******")
+print()
+
+#A[6]={1,2,3,4,5,6}<--para remplazar esta función que es una rreglo en el lenguaje de programación en C.
+
+Alumnos=["Rosalinda","Juan","Lourdes","Tania","Bryan","Rebeca","Jennyfer","Hector","Galilea","Patricia","Jesus","Addi"]#Así se sustituye en phyton (Es un arreglo).
+
+for Alumno in Alumnos:#Declaración de el ciclo for.
+    print(f"Hola {Alumno}")#Imprime la palabra hola acompañado de el arreglo.
+
+    #for i in range (1,11):
+       # print(i)
