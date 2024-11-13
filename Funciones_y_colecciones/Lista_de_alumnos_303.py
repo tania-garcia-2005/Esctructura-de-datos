a7d56e72c116dc78927dab2651d6549622ef8635
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-11-12 22:14:33 -0600

    2

---
+++ Funciones_y_colecciones/Lista_de_alumnos_303.py
@@ -0,0 +1,41 @@
+print("****************************************")
+print("****   Tania Garcia Flores.        *****")
+print("****   11 de Noviembre de 2024.    *****")
+print("****************************************")
+
+'''''
+Descripción:Realiza la 
+
+'''
+
+print()
+print("****** Lista alumnos ******")
+print()
+alumnos=[]
+alumnos.append("Hector")
+alumnos.append("Tania")
+alumnos.append("Addi")
+alumnos.append("Alberto")
+alumnos.append("Juan")
+alumnos.append("Rosalidna")
+alumnos.append("Bryan")
+alumnos.append("Jamileth")
+alumnos.append("Galilea")
+alumnos.append("Jennyfer")
+alumnos.append("Patricia")
+len(alumnos)
+for a in alumnos:
+    print(a)
+print()
+print("----------------------------------------------------")
+
+alumnos.sort()
+for z in alumnos:
+    print(z)
+print()
+print("----------------------------------------------------")
+alumnos.sort(reverse=True)
+for u in alumnos:
+    print(u)
+print()
+print("----------------------------------------------------")
