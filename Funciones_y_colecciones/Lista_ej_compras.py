61d79a0604f12953b79fa557955e388634ff95cf
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-11-14 23:00:40 -0600

    2

--- Funciones_y_colecciones/Lista_ej_compras.py
+++ Funciones_y_colecciones/Lista_ej_compras.py
@@ -1,6 +1,6 @@
 print("****************************************")
 print("****   Tania Garcia Flores.        *****")
-print("****   13 de Noviembre de 2024.    *****")
+print("****   14 de Noviembre de 2024.    *****")
 print("****************************************")
 
 '''''
@@ -8,38 +8,57 @@ Descripción:Añadir producto a la lista de compras.
 
 '''
 
-print()
-print("****** Lista de compras ******")
-print()
 
 def menu ():
-    lista = []
-    opcion=0
 
     print("1)Ver lista")
-    print("2) Añadir producto a la lista")#nombre y cantidad
+    print("2)Añadir producto a la lista")#nombre y cantidad
     print("3)Eliminar producto de lista")
     print("0)Salir")
 
     print()
-    opcion = int(input("ingrese su opcion:"))
+    opcion = int(input("ingrese su opción:"))
 
     return opcion
 
-def lista(opcion,producto1,producto2):
+def  añadir_producto(lista_de_compras):
 
-    if opcion=1:#para ver ña lista
-
-numero = input("Ingrese su video")
-lista.insert(-1, numero)
-print("----------------------------------------------------")
+    print()
+###################################################################
+print()
+print("****** Lista de compras ******")
 print()
 
-    elif opcion==2:#Añadirproducto a ala lista
+lista = []
+opcion = None  # None sustituye al -1,para tener una variable opuesta a tu condiicioon del while(no tiene ningun valro)
+while opcion!=0:
+    opcion = menu()
 
+
+    if opcion==1:
+        print(lista)
+        print("----------------------------------------------------")
+        print()
+    elif opcion == 2:
+        nombre=input("nombre del producto")#ingresa el nombre del producto
+        cantidad=input("cantidad del producto")#ingresa la cantidad del producto
+
+        producto=[nombre,cantidad]#
+        lista.append(producto)
         print("----------------------------------------------------")
         print()
 
-    elif opcion==3:#Elimina el producot de la lisa
-        print("----------------------------------------------------")
+    elif opcion ==3:
         print()
+    elif opcion ==
+         print()
+        numero=int(input("ingrese su cantidad"))
+        i=1
+    while i<= numero:
+        ingresar=input("")
+    print("----------------------------------------------------")
+    print()
+
+
+
+#tengo 2 listas con 2 indices y tengo que ver cpomo borarr las listas para eliminar un rpoducto
