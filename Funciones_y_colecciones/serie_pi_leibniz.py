a3010561d368007f90832260b784d752484772c5
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-11-19 15:58:10 -0600

    2

---
+++ Funciones_y_colecciones/serie_pi_leibniz.py
@@ -0,0 +1,49 @@
+print("****************************************")
+print("****   Tania Garcia Flores.        *****")
+print("****   14 de Noviembre de 2024.    *****")
+print("****************************************")
+
+'''''
+Descripción:. 
+
+'''
+
+###################################################################
+print()
+print("****** serie de pi******")
+print()
+
+#serie de pi leibniz
+
+pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
+print(f"El valor de pi considerando 3 elementos:{sum (pi_serie[0:2])}")
+pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
+print(f"El valor de pi considerando 5 elementos:{sum (pi_serie[0:4])}")
+pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
+print(f"El valor de pi considerando 8 elementos:{sum (pi_serie[0:7])}")
+pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
+print(f"El valor de pi considerando 10 elementos:{sum (pi_serie[0:9])}")
+pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
+print(f"El valor de pi considerando todos los elementos:{sum (pi_serie)}")
+
+###################################################################
+print()
+print("****** Ejemplo de cordenadas ******")
+print()
+
+punto1=(1,0)
+punto2=(5,3)
+
+print("cordenadas en tuplas:{{y}}")
+#Desempaquetaod de tuplas
+x1,y1=punto1
+x2,y2=punto2
+
+
+
+print(f"La expresion de la recta es: y={m{x+{b}}}")
+m=(y2-y1)(x2-x1)
+b=y1-mx1
+
+
+
