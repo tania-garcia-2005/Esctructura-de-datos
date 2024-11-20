373a387e4561fd9321666af99823ccf9c0a83cfc
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-11-20 15:03:34 -0600

    2

--- Funciones_y_colecciones/serie_pi_leibniz.py
+++ Funciones_y_colecciones/serie_pi_leibniz.py
@@ -8,7 +8,7 @@ Descripción:.
 
 '''
 
-###################################################################
+print("------------------------------------------------------------------------")
 print()
 print("****** serie de pi******")
 print()
@@ -16,34 +16,32 @@ print()
 #serie de pi leibniz
 
 pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
-print(f"El valor de pi considerando 3 elementos:{sum (pi_serie[0:2])}")
+print(f"El valor de pi considerando 3 elementos:{sum (pi_serie[0:2]):.4f}")
 pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
-print(f"El valor de pi considerando 5 elementos:{sum (pi_serie[0:4])}")
+print(f"El valor de pi considerando 5 elementos:{sum (pi_serie[0:4]):.4f}")
 pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
-print(f"El valor de pi considerando 8 elementos:{sum (pi_serie[0:7])}")
+print(f"El valor de pi considerando 8 elementos:{sum (pi_serie[0:7]):.4f}")
 pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
-print(f"El valor de pi considerando 10 elementos:{sum (pi_serie[0:9])}")
+print(f"El valor de pi considerando 10 elementos:{sum (pi_serie[0:9]):.4f}")
 pi_serie=(4,-4/3,4/5,-4/7,4/9,-4/11,4/13,-4/15,4/17,-4/19,4/21,-4/23)
-print(f"El valor de pi considerando todos los elementos:{sum (pi_serie)}")
-
-###################################################################
+print(f"El valor de pi considerando todos los elementos:{sum (pi_serie):.4f}")
+print("------------------------------------------------------------------------")
 print()
 print("****** Ejemplo de cordenadas ******")
 print()
 
 punto1=(1,0)
 punto2=(5,3)
-
-print("cordenadas en tuplas:{{y}}")
+print()
+print(f"cordenadas en tuplas:{punto1}{punto2}")
 #Desempaquetaod de tuplas
 x1,y1=punto1
 x2,y2=punto2
 
-
-
-print(f"La expresion de la recta es: y={m{x+{b}}}")
-m=(y2-y1)(x2-x1)
-b=y1-mx1
+pendiente=(y2-y1)/(x2-x1)
+b=y1-pendiente*x1
+print(f"La expresion de la recta es: y={pendiente}x{b}")
+print("------------------------------------------------------------------------")
 
 
 
