f752651ba3990a4b6df9cce4d0db67bfb49a4239
Tania Garcia Flores <taniagarciaflrs@gmail.com>
2024-11-21 12:30:49 -0600

    2

--- Ciclos_while/Calculadora.py
+++ Ciclos_while/Calculadora.py
@@ -16,7 +16,7 @@ print("-----------------------------------------------------------------")
 print()
 print()
 
-
+##Declaración del menú correspondiente.
 
 opcion = None
 while opcion != 0:
@@ -29,38 +29,34 @@ while opcion != 0:
     print("5) División entera.")
     print("6) Exponenciación.")
     opcion = int(input("Ingrese una opción: "))
+    #operaciones
 
-    if opcion == 1:
+    if opcion == 1:  # Este punto ejecuta la Opción 1 suma.
         numero_uno, numero_dos = float(input("Ingrese un número: ")), float(input("Ingrese otro número: "))
         print(f"La suma de {numero_uno} y {numero_dos} es {numero_uno + numero_dos:.2f}.")
         print()
-    elif opcion == 2:
+    elif opcion == 2:  #Este punto ejecuta la Opción 2 resta.
         numero_uno, numero_dos = float(input("Ingrese un número: ")), float(input("Ingrese otro número: "))
         print(f"La resta de {numero_uno} y {numero_dos} es {numero_uno - numero_dos:.2f}.")
         print()
-    elif opcion == 3:
+    elif opcion == 3:  #Este punto ejecuta la Opción 3 multiplicación.
         numero_uno, numero_dos = float(input("Ingrese un número: ")), float(input("Ingrese otro número: "))
         print(f"La multiplicación de {numero_uno} y {numero_dos} es {numero_uno * numero_dos:.2f}.")
         print()
-    elif opcion == 4:
+    elif opcion == 4:  #Este punto ejecuta la Opción 4 división
         numero_uno, numero_dos = float(input("Ingrese un número: ")), float(input("Ingrese otro número: "))
-        if numero_dos != 0:
-            print(f"La división entre {numero_uno} y {numero_dos} es: {numero_uno / numero_dos:.2f}.")
-        else:
-            print("Error: No se puede dividir entre cero.")
+        print(f"La división entre {numero_uno} y {numero_dos} es: {numero_uno / numero_dos:.2f}.")
         print()
-    elif opcion == 5:
+    elif opcion == 5:  #Este punto ejecuta la Opción 5 división entera
         numero_uno, numero_dos = float(input("Ingrese un número: ")), float(input("Ingrese otro número: "))
-        if numero_dos != 0:
-            print(f"La división entera entre {numero_uno} y {numero_dos} es {numero_uno // numero_dos:.2f}.")
-        else:
-            print("Error: No se puede dividir entre cero.")
+        print(f"La división entera entre {numero_uno} y {numero_dos} es {numero_uno // numero_dos:.2f}.")
         print()
-    elif opcion == 6:  # Corrige el número de opción para exponenciación
+    elif opcion == 6:  #Este punto ejecuta la Opción 6 exponenciación
         numero_uno, numero_dos = float(input("Ingrese un número: ")), float(input("Ingrese otro número: "))
         print(f"La exponenciación de {numero_uno} a la {numero_dos} es {numero_uno ** numero_dos:.2f}.")
         print()
+
     elif opcion == 0:
-        print("Saliendo del programa.")
+        print("Usted ha salido exitosamente del programa.")
     else:
-        print("Opción no válida. Intente de nuevo.")
+        print("La opción no es válida. Intente de nuevamente por favor.")
