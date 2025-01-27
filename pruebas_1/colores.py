3fd18b0416adec641832058072f4f20916cbd3b8
HP-User <taniagarciaflrs@gmail.com>
2025-01-27 17:45:02 -0600

    new

---
+++ pruebas_1/colores.py
@@ -0,0 +1,25 @@
+import time
+
+# Códigos ANSI que cambian el color de mis letras.
+MORADO = "\033[38;5;128m"  # Morado claro
+AZUL = "\033[34m"  # Azul
+AMARILLO = "\033[33m"  # Amarillo
+VERDE = "\033[32m"  # Verde
+RESET = "\033[0m"  # esto hace que se vuelva al color inicial.
+def mostrar_mensaje(mensaje, retraso=0.1):#aquí en retraso pongo la velocidad que quiero, y entre más ceros le ponga será más rapido.
+    colores = [MORADO, AZUL, AMARILLO, VERDE]  #Colores que ocupare.
+    for i, caracter in enumerate(mensaje):
+        color = colores[i % len(colores)]  # Cicla entre los colores
+        print(color + caracter, end='', flush=True)  # Aplica el color a las letras.
+        time.sleep(retraso)
+    print(RESET)  #Esto vuelve al color original.
+
+#Aquí solo agrego el texto que quiero que se muestre en la consola.
+mensaje = """Me quieren agitar,
+Me incitan a gritar,
+Soy como una roca,
+Palabras no me tocan,
+Adentro hay un volcán,
+Que pronto va a estallar,
+Yo quiero estar tranquilo."""
+mostrar_mensaje(mensaje)
